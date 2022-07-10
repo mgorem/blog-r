adc98bee1fcc123b92b213f9e2972db44269b806
mgorem <oremgitonga22@gmail.com>
2022-07-10 18:30:40 +0300

    delete blog

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 import useFetch from './useFetch'
 
 const BlogDetails = () => {
 
     const { id } = useParams()
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory()
+
+    const handleDelete = () => {
+      fetch('http://localhost:8000/blogs/' + blog.id, {
+        method: 'DELETE'
+      }).then(() => {
+        history.push('/')
+      })
+    }
 
   return (
     <div className='blog-details'>
@@ -16,6 +26,7 @@ const BlogDetails = () => {
                 <h2>{ blog.title }</h2>
                 <p>Written By: { blog.author }</p>
                 <div>{ blog.body }</div>
+                <button onClick={handleDelete}>Delete</button>
             </article>
         ) }
     </div>
