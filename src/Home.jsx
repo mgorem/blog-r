2d98b2feed8949f3b68871c0b133dd86088d790a
mgorem <oremgitonga22@gmail.com>
2022-07-10 10:04:45 +0300

    fetch data, conditions, handle errors

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,26 +4,36 @@ import BlogList from './BlogList'
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null)
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch(' http://localhost:8000/blogs')
-    .then(res => {
-      return res.json()
-    })
-    .then(data => {
-      console.log(data)
-      setBlogs(data)
-    })
+    setTimeout(() =>{
+      fetch(' http://localhost:8000/blogs')
+        .then(res => {
+          if(!res.ok) {
+            throw Error('Could not feth data from that resource.')
+          }
+          return res.json()
+        })
+        .then(data => {
+          console.log(data)
+          setBlogs(data)
+          setIsPending(false)
+          setError(null)
+        })
+        .catch(err => {
+          setIsPending(false)
+          setError(err.message)
+        })
+  }, 1000)
   }, [])
 
   return (
     <div className='home'>
-      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+      {error && <div>{ error }</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs"/> */}
     </div>
   )
