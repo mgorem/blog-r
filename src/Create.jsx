86bf15f3e62c97c2f543b7e7998af9d2329133aa
mgorem <oremgitonga22@gmail.com>
2022-07-10 17:16:37 +0300

    controled inputs/forms, controlled select dropdown

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('mario')
+
   return (
     <div className='create'>
     <h2>Add a New Blog</h2>
+    <form>
+      {/* Title */}
+      <label>Blog Title:</label>
+      <input 
+        type="text" 
+        required
+        value={ title }
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      {/* Textarea */}
+      <label>Blog Body:</label>
+      <textarea 
+        required
+        value={ body }
+        onChange={(e) => setBody(e.target.value)}
+        style={{height:"300px"}}
+      ></textarea>
+
+      {/* Author */}
+      <label>Blog Author:</label>
+      <select
+        value={ author }
+        onChange={(e) => setAuthor(e.target.value)}
+      >
+        <option value="mario">mario</option>
+        <option value="yoshi">yoshi</option>
+      </select>
+      <button>Add Blog</button>
+      <p>{ title }</p>
+      <p>{ body }</p>
+      <p>{ author }</p>
+    </form>
     </div>
   )
 }
