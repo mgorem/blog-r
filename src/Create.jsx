424052a20898ccf5f4efb5d6dee020e262667e23
mgorem <oremgitonga22@gmail.com>
2022-07-10 17:59:17 +0300

    make post request, add blog

--- src/Create.jsx
+++ src/Create.jsx
@@ -5,12 +5,22 @@ const Create = () => {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('mario')
+  const [isPending, setIsPending] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const blog = { title, body, author }
 
-    console.log(blog)
+    setIsPending(true)
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('New Blog Added')
+      setIsPending(false)
+    })
   }
 
   return (
@@ -44,10 +54,8 @@ const Create = () => {
         <option value="mario">mario</option>
         <option value="yoshi">yoshi</option>
       </select>
-      <button>Add Blog</button>
-      <p>{ title }</p>
-      <p>{ body }</p>
-      <p>{ author }</p>
+      { !isPending && <button>Add Blog</button> }
+      { isPending && <button disabled style={{backgroundColor:"gray", color:"#000"}}>Adding Blog...</button> }
     </form>
     </div>
   )
