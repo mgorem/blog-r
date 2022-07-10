8dba529f04b4a6fa465bce02c51cce00115ded94
mgorem <oremgitonga22@gmail.com>
2022-07-10 17:29:58 +0300

    submit events from forms-create new object

--- src/Create.jsx
+++ src/Create.jsx
@@ -6,10 +6,17 @@ const Create = () => {
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('mario')
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const blog = { title, body, author }
+
+    console.log(blog)
+  }
+
   return (
     <div className='create'>
     <h2>Add a New Blog</h2>
-    <form>
+    <form onSubmit={handleSubmit}>
       {/* Title */}
       <label>Blog Title:</label>
       <input 
